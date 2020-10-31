247d4da586334c0e5e8fb6db58e274a75b0e917b
TolkachevPeter <peter.tolkachev@gmail.com>
2020-10-31 22:54:11 +0300

    add btn

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
-    <div class="main" v-for="image in allImages" :key="image.id">
+  <div class="main">
+    <div v-for="image in allImages" :key="image.id">
       <h2>{{ image.title }}</h2>
       <p>{{ image.import_datetime }}</p>
       <v-img max-height="253" max-width="453" v-bind:src="image.image_url"></v-img>
     </div>
+    <v-btn class='home__btn'
+    v-on:click="fetchImages" color="primary" elevation="2">Загрузить</v-btn>
   </div>
 </template>
 
@@ -24,6 +26,11 @@ export default {
 .main {
   max-width: 500px;
   justify-content: center;
+  text-align: center;
   margin: 79px auto 79px;
+}
+.home__btn {
+  margin: 40px auto 79px;
+  max-width: 200px;
 }
 </style>
