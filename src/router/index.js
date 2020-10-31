30cc674e2c752f7a8d609aa9936f89e05ec055e0
TolkachevPeter <peter.tolkachev@gmail.com>
2020-10-31 16:50:45 +0300

    add routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,22 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '@/components/Home.vue';
+import History from '@/components/History.vue';
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: '',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/history',
+    name: 'history',
+    component: History,
+  },
+];
 
 const router = new VueRouter({
   routes,
