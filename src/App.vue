1aeecded9c465e74bff2174a20010c1ffde262df
TolkachevPeter <peter.tolkachev@gmail.com>
2020-10-31 16:22:42 +0300

    add vuetify links

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,65 @@
 <template>
-  <div id="app">
-  </div>
+<v-app>
+  <v-navigation-drawer
+   app
+  temporary
+  v-model="drawer"
+  >
+    <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="link in links"
+          :key="link.title"
+          :to='link.url'
+        >
+
+          <v-list-item-content>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+    <v-app-bar
+      absolute
+      color="white"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+    >
+      <v-app-bar-nav-icon
+      @click='drawer = !drawer'
+      ></v-app-bar-nav-icon>
+
+  <v-tabs>
+    <v-tab
+  v-for='link in links'
+  :key='link.title'
+  :to='link.url'>{{link.title}}</v-tab>
+  </v-tabs>
+    </v-app-bar>
+  </v-app-bar>
+
+  <v-main>
+      <router-view></router-view>
+  </v-main>
+
+</v-app>
 </template>
 
-<style>
+<script>
 
-</style>
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { title: 'Главная', url: '/' },
+        { title: 'История', url: '/history' },
+      ],
+    };
+  },
+};
+</script>
