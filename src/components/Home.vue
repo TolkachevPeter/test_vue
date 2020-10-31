40d3d3313032761d9488630b0d4e4c34e098c475
TolkachevPeter <peter.tolkachev@gmail.com>
2020-10-31 17:14:18 +0300

    add slider in Home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,60 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Home</h1>
-      </v-flex>
-    </v-layout>
-  </v-container>
+    <div class='main'>
+    <v-carousel v-model="model" hide-delimiters :show-arrows="false">
+      <v-carousel-item
+        v-for="(color, i) in colors"
+        :key="color"
+      >
+        <v-sheet
+          :color="color"
+          height="100%"
+          tile
+        >
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <div class="display-3">
+              Slide {{ i + 1 }}
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+        <v-row justify="space-around">
+      <v-icon @click="model--">
+        mdi-minus
+      </v-icon>
+      {{ model }}
+      <v-icon @click="model++">
+        mdi-plus
+      </v-icon>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-
+      colors: [
+        'primary',
+        'secondary',
+        'yellow darken-2',
+        'red',
+        'orange',
+      ],
+      model: 0,
     };
   },
 };
 </script>
+
+<style scoped>
+.main{
+  max-width: 1000px;
+  justify-content: center;
+  margin: 79px auto 79px;
+}
+</style>
