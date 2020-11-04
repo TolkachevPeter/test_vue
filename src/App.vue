7c1176d2f065c61c37034254f83880fe33c91539
TolkachevPeter <peter.tolkachev@gmail.com>
2020-11-04 13:40:30 +0300

    fix errors

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,38 @@
 <template>
-<v-app>
-  <v-navigation-drawer
-   app
-  temporary
-  v-model="drawer"
-  >
-    <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="link in links"
-          :key="link.title"
-          :to='link.url'
-        >
-
+  <v-app>
+    <v-navigation-drawer app temporary v-model="drawer">
+      <v-list dense nav>
+        <v-list-item v-for="link in links" :key="link.title" :to="link.url">
           <v-list-item-content>
             <v-list-item-title>{{ link.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
-  </v-navigation-drawer>
+    </v-navigation-drawer>
 
-  <v-app-bar app>
-    <v-app-bar
-      absolute
-      color="white"
-      elevate-on-scroll
-      scroll-target="#scrolling-techniques-7"
-    >
-      <v-app-bar-nav-icon
-      class="hidden-md-and-up"
-      @click='drawer = !drawer'
-      ></v-app-bar-nav-icon>
+    <v-app-bar app>
+      <v-app-bar absolute color="white" elevate-on-scroll scroll-target="#scrolling-techniques-7">
+        <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-  <v-tabs>
-    <v-tab class="hidden-sm-and-down"
-  v-for='link in links'
-  :key='link.title'
-  :to='link.url'>{{link.title}}</v-tab>
-  </v-tabs>
+        <v-tabs>
+          <v-tab
+            class="hidden-sm-and-down"
+            v-for="link in links"
+            :key="link.title"
+            :to="link.url"
+            >{{ link.title }}</v-tab
+          >
+        </v-tabs>
+      </v-app-bar>
     </v-app-bar>
-  </v-app-bar>
 
-  <v-main>
+    <v-main>
       <router-view></router-view>
-  </v-main>
-
-</v-app>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
